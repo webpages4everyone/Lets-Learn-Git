e78dc26af78b8159c2307b926f2240ed3db874c1
wasid <wasid.bcss19@iba-suk.edu.pk>
2022-10-26 11:39:15 +0500

    another diff commit.

---
+++ Diff.java
@@ -0,0 +1,10 @@
+public Diff{
+    public void static main(String [] args){
+
+        int a = 5;
+        int b = 2;
+
+        System.out.println("The overall difference is: "+ (a-b));
+
+    }
+}
